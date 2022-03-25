e6d62fa5e9dbefad1f203c97c5012fd9df1bd54c
Maurice <maurice.baez1@gmail.com>
2022-03-24 22:56:50 -0700

    added functionality to choose different stories

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
+from stories import stories
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def render_home():
+
+    story_list = [story_type for story_type in stories.keys()]
+
+
+    return render_template("home.html", stories = story_list)
 
 
 @app.get("/questions")
@@ -14,7 +23,11 @@ def get_questions():
 
     """ Generating form prompts"""
 
-    prompts = silly_story.prompts
+    story_type = request.args.get("story-type")
+
+    prompts = stories[story_type].prompts
+
+    session["story"] = story_type
 
     return render_template("questions.html", words=prompts)
 
@@ -24,17 +37,10 @@ def get_form_data():
 
     """ Generate story from form data """
 
+    story_type = session["story"]
+
     form_data = request.args
 
-    answers  = silly_story.generate(form_data)
+    answers  = stories[story_type].generate(form_data)
 
     return render_template("story.html", answers = answers)
-# all logic to get form data goes here
-#return render template story.html
-
-
-
-
-
-
-#loop through the form array and generate the html input for each
